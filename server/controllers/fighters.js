d948c3cfb440b6368d345ae3a4a7b2538d3b7ab6
Robert Hubert <robertfhubert@gmail.com>
2021-05-08 13:57:15 -0400

    add fighter-adding-script

--- server/controllers/fighters.js
+++ server/controllers/fighters.js
@@ -5,61 +5,61 @@ const fs = require('fs');
 const { Fighter, LeagueFighter, Player, sequelize, LeagueAuction } = require('../models');
 
 router.post('/all', async(req, res) => {
-    // fs.createReadStream('/Users/roberthubert/Desktop/mma-fantasy/csv-doc.csv')
-    //     .pipe(csv())
-    //     .on('data', async(row) => {
-    //         console.log(row);
-    //      //   debugger;
-    //         let wins = ''
-    //         let losses = '';
-    //         let seenDash = false
-    //         for (let i = 0; i < row.RECORD.length; i++){
-    //             if (isNaN(parseInt(row.RECORD[i]))){
-    //                 if (seenDash){
-    //                     break;
-    //                 }
-    //                 seenDash = true;
-    //             }
-    //             if (!seenDash && !isNaN(parseInt(row.RECORD[i]))){
-    //                 wins += row.RECORD[i];
-    //             } else if (seenDash && !isNaN(parseInt(row.RECORD[i]))){
-    //                 losses += row.RECORD[i];
-    //             }
-    //         }
-    //         let nameArr = row.NAME.split(' ');
-    //         let first = nameArr[0];
-    //         let last = nameArr[1];
-    //         let ranking;
-    //         if (parseInt(row.RANK)){
-    //             ranking = parseInt(row.RANK)
-    //         } else {
-    //             ranking = null
-    //         }
-    //         let fighterObject = {
-    //             firstName: first,
-    //             lastName: last,
-    //             fullName: row.NAME.replace(/([^a-z0-9 ]+)/gi, ''),
-    //             wins: parseInt(wins),
-    //             losses: parseInt(losses),
-    //             ranking: ranking,
-    //             nextOpponent: row.OPPONENT,
-    //             lastWeight: row.WEIGHTCLASS
-    //         }
-    //         console.log(fighterObject);
+    fs.createReadStream('/Users/roberthubert/Desktop/mma-fantasy/csv-doc.csv')
+        .pipe(csv())
+        .on('data', async(row) => {
+            console.log(row);
+         //   debugger;
+            let wins = ''
+            let losses = '';
+            let seenDash = false
+            for (let i = 0; i < row.RECORD.length; i++){
+                if (isNaN(parseInt(row.RECORD[i]))){
+                    if (seenDash){
+                        break;
+                    }
+                    seenDash = true;
+                }
+                if (!seenDash && !isNaN(parseInt(row.RECORD[i]))){
+                    wins += row.RECORD[i];
+                } else if (seenDash && !isNaN(parseInt(row.RECORD[i]))){
+                    losses += row.RECORD[i];
+                }
+            }
+            let nameArr = row.NAME.split(' ');
+            let first = nameArr[0];
+            let last = nameArr[1];
+            let ranking;
+            if (parseInt(row.RANK)){
+                ranking = parseInt(row.RANK)
+            } else {
+                ranking = null
+            }
+            let fighterObject = {
+                firstName: first,
+                lastName: last,
+                fullName: row.NAME.replace(/([^a-z0-9 ]+)/gi, ''),
+                wins: parseInt(wins),
+                losses: parseInt(losses),
+                ranking: ranking,
+                nextOpponent: row.OPPONENT,
+                lastWeight: row.WEIGHTCLASS
+            }
+            console.log(fighterObject);
 
-    //     //     console.log({'wins': parseInt(wins), 'losses': parseInt(losses)});
-    //     //    //console.log(recordArr[1])
-    //        try {
-    //            let fighter = await Fighter.create(fighterObject)
-    //             console.log(fighter)
-    //        } catch(error){
-    //            console.log(error)
-    //        }
-        // })
-        // .on('end', () => {
-        //     console.log('CSV file successfully processed');
-        // })
-       // .catch((err) => console.log(err));
+        //     console.log({'wins': parseInt(wins), 'losses': parseInt(losses)});
+        //    //console.log(recordArr[1])
+           try {
+               let fighter = await Fighter.create(fighterObject)
+                console.log(fighter)
+           } catch(error){
+               console.log(error)
+           }
+        })
+        .on('end', () => {
+            console.log('CSV file successfully processed');
+        })
+       .catch((err) => console.log(err));
     //debugger;
     let {selectedWeightClass} = req.body;
     //let selectedWeightClass = arr[0];
